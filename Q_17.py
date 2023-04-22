e4958b42095505a4775afa21e28bc90b97618f1c
anandshivam007 <61194032+anandshivam007@users.noreply.github.com>
2023-04-23 01:08:55 +0530

    Delete Q_17.py

--- Q_17.py
+++
@@ -1,4 +0,0 @@
-#*Write a program to read in 10 numbers and compute the sum, average,maximum, second maximum and minimum values
-
-numbers = (input("Enter 10 Numbers separated by comma : "))
-print(numbers)
